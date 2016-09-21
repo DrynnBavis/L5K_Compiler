34d2a298d299acec15517f60c2b8bd4c2fdc18ab
Brynn <drynnbavis@gmail.com>
2016-09-21 15:19:07 -0400

    fixed the missing modules

--- L5K_Compiler/Form1.cs
+++ L5K_Compiler/Form1.cs
@@ -143,6 +143,372 @@ namespace L5K_Compiler
 					OutputData (Class := Standard) := [0,0.00000000e+000,0.00000000e+000,0.00000000e+000];
 			END_CONNECTION
 
+	END_MODULE
+
+	MODULE $NoName (Parent := ""_xxxx_01M02"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""RHINOBP-DRIVE-PERIPHERAL-MODULE"",
+	            Vendor := 1,
+	            ProductType := 0,
+	            ProductCode := 28,
+	            Major := 1,
+	            Minor := 1,
+	            UserDefinedVendor := 1,
+	            UserDefinedProductType := 142,
+	            UserDefinedProductCode := 57760,
+	            UserDefinedMajor := 1,
+	            UserDefinedMinor := 1,
+	            Slot := 5,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 1,
+	            KeyMask := 2#0000_0000_0001_1111,
+	            ShutdownParentOnFault := 0,
+	            DrivesADCMode := 1,
+	            DrivesADCEnabled := 0,
+	            UserDefinedCatalogNumber := ""EtherNet/IP"")
+			ExtendedProp := [[[___<public><Port5CCV>0</Port5CCV><Port5CCVInfo>1,0,0,0,0,0,0,0,0,</Port5CCVInfo><Port5HLP_Size>0</Port5HLP_Size><Port5Type>EtherNet/IP</Port5Type><ConfigID>100</ConfigID><Port5DeviceDefinition>0001000000A0000000E1000100000000000100010101    0001010100010000</Port5DeviceDefinition></public>___]]]
+			ConfigData := [8,6,1];
+
+            ConfigScript(Size := 560) := [44,2,0,0,4,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,8,-106,0,0,0,1,0,0,0,1,0,0,0,8,0,0,0,75,2,32,-110,36,0,-1,-1,0,0,0,126,0,0,0,8,20,0,0,0,1,0,0,0,1,0,0,0
+		,11,0,0,0,16,4,32,-109,37,0,0,0,48,2,3,1,0,0,0,11,0,0,0,16,4,32,-109,37,0,5,0,48,9,1,1,0,0,0,11,0,0,0,16,4,32,-109,37,0,7,0,48,9,-64,1,0
+		,0,0,11,0,0,0,16,4,32,-109,37,0,8,0,48,9,-88,1,0,0,0,11,0,0,0,16,4,32,-109,37,0,10,0,48,9,2,1,0,0,0,14,0,0,0,16,4,32,-110,37,0,0,0,48,31
+		,2,0,0,0,0,0,-8,0,0,0,8,21,0,0,0,1,0,0,0,1,0,0,0,11,0,0,0,16,4,32,-97,37,0,0,0,48,2,3,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,1,0,48,9,23,2,0,0,1,0,0,0
+		,14,0,0,0,16,4,32,-97,37,0,2,0,48,9,25,2,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,17,0,48,9,7,0,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,18,0,48
+		,9,-73,3,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,19,0,48,9,26,0,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,20,0,48,9,8,2,0,0,1,0,0,0,14,0,0,0,16,4,32
+		,-97,37,0,21,0,48,9,11,0,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,22,0,48,9,-94,1,0,0,1,0,0,0,14,0,0,0,16,4,32,-97,37,0,23,0,48,9,-5,0,0,0,1
+		,0,0,0,14,0,0,0,16,4,32,-97,37,0,24,0,48,9,-91,3,0,0,45,0,0,0,8,101,0,0,0,1,0,0,0,6,0,0,0,24,0,0,0,16,3,32,-110,36,0,48,38,0,0,0,0,0,0,0,0,0
+		,0,0,0,0,0,0,0,64,0,0,0,0,0,0,36,0,0,0,8,61,0,0,0,1,0,0,0,1,0,0,0,11,0,0,0,16,4,32,-105,37,0,0,0,48,3,3,3,0,0,0,-56,-81,0,0,29,0,0,0,0,100,0,0,0
+		,2,0,0,0,-128,0,0,0,-81,96,99,-35,51,-23,17,-26,-121,-33,0,12,41,-25,-114,-55,0,0,0];
+	END_MODULE
+
+    MODULE $NoName(Parent := ""_xxxx_01M02"",
+                ParentModPortId := 1,
+                CatalogNumber := ""RHINOBP-DRIVE-PERIPHERAL-MODULE"",
+                Vendor := 1,
+                ProductType := 0,
+                ProductCode := 28,
+                Major := 1,
+                Minor := 1,
+                UserDefinedVendor := 1,
+                UserDefinedProductType := 142,
+                UserDefinedProductCode := 33184,
+                UserDefinedMajor := 2,
+                UserDefinedMinor := 5,
+                Slot := 14,
+                Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 0,
+                KeyMask := 2#0000_0000_0000_0000,
+	            ShutdownParentOnFault := 0,
+                DrivesADCMode := 1,
+                DrivesADCEnabled := 0,
+                UserDefinedCatalogNumber := ""DeviceLogix"")
+
+            ExtendedProp := [[[___<public><Port14DeviceDefinition>0001000000A000000081000100000000000100010502    0001050200010000</Port14DeviceDefinition><Port14CCV>0</Port14CCV><Port14CCVInfo>1,0,0,0,0,0,0,0,0,</Port14CCVInfo><Port14HLP_Size>40</Port14HLP_Size><Port14HLP_DataBlock1>1F0002000000010008000000010001000000000028000E0000000100FF0300000000000000000000</Port14HLP_DataBlock1><Port14Type>DeviceLogix</Port14Type><ConfigID>100</ConfigID></public>___]]]
+			ConfigData := [8,6,1];
+
+            ConfigScript(Size := 1452) := [-88,5,0,0,4,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,8,-106,0,0,0,1,0,0,0,1,0,0,0,8,0,0,0,75,2,32,-110,36,0,-1,-1,0,0,0,28,0,0,0,8,60,0,0,0,1,0,0,0,1,0,0,0
+		,11,0,0,0,16,4,32,-105,37,0,0,0,48,3,1,-41,4,0,0,8,11,0,0,0,1,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,2,0,3,0,0,2,1,3,0,0,0,0,0,0,0,0,0,0,0
+		,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,3,0,1,0,0,31,36,2,0
+		,16,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0
+		,0,0,16,3,32,-96,36,1,48,5,4,0,3,1,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0
+		,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,5,0,3,12,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0
+		,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,6,0,3,23,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4
+		,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,7,0,3,34,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0
+		,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,8,0,3,45,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,3,52
+		,0,9,4,0,0,0,0,4,5,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,9,0,3,58,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0
+		,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,10,0,3,69,0,9,4,0,0,0
+		,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,11
+		,0,3,80,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96
+		,36,1,48,5,12,0,3,91,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0
+		,0,0,16,3,32,-96,36,1,48,5,13,0,3,102,0,9,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,14,0,1,0,0,31,4,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,15,0,46,13,0,2,10,0,-128,5,3,33,0,14,3,36,1,0,46,13,0,2,10,0,-128,5,3,33
+		,0,62,3,36,0,0,46,13,0,2,8,0,-128,81,2,32,55,36,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,74,0,0,0,16,3,32,-96,36,1,48,5,16,0,46,13,0,2
+		,24,0,-128,76,2,32,55,36,1,3,0,0,0,1,0,-1,1,1,0,-113,0,-96,-127,2,5,0,46,13,0,2,15,0,-128,80,2,32,55,36,1,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0
+		,0,0,0,0,0,0,0,45,0,0,0,8,101,0,0,0,1,0,0,0,6,0,0,0,24,0,0,0,16,3,32,-110,36,0,48,38,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,64,0,0,0,0,0,0,36,0,0,0,8,61,0
+		,0,0,1,0,0,0,1,0,0,0,11,0,0,0,16,4,32,-105,37,0,0,0,48,3,3,3,0,0,0,-56,-81,0,0,29,0,0,0,0,100,0,0,0,2,0,0,0,-128,0,0,0,-81,96,99,-36,51,-23
+		,17,-26,-121,-33,0,12,41,-25,-114,-55,0,0,0];
+	END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        string[] mPowerFlex755 = @"	MODULE _xxxx_01M05 (Description := ""CIP Control"",
+
+                        Parent := ""Drives"",
+	                    ParentModPortId := 2,
+	                    Vendor := 1,
+	                    ProductType := 37,
+	                    ProductCode := 1,
+	                    Major := 3,
+	                    Minor := 1,
+	                    PortLabel := ""ENet"",
+	                    NodeAddress := ""192.168.0.5"",
+	                    Mode := 2#0000_0000_0000_0000,
+	                    CompatibleModule := 0,
+	                    KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>101</ConfigID><FeedbackDevice1>1</FeedbackDevice1><FeedbackDevice2>2</FeedbackDevice2></public>___]]]
+			ConfigData := [224,1,257,8,11158631,0,10000,16908804,0,1,-65536,1120403456,1045220557,0,0,0,0,1120403456,0,1120403456
+		,0,1109393408,2,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8192004,8192125,67305985,0,0,0,0,0,0,0];
+			CONNECTION MotionDiagnostics(Rate := 1000,
+                                          EventID := 0)
+
+                    InputData(Class := Standard) := [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			END_CONNECTION
+
+            CONNECTION MotionSync(Rate := 2000,
+                                   EventID := 0)
+
+            END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] mPowerFlex755EENETCM = @"	MODULE _xxxx_xxMxx (Description := ""FS Length Adj LH"",
+
+                        Parent := ""Drives"",
+	                    ParentModPortId := 2,
+	                    CatalogNumber := ""PowerFlex 755-EENET-CM"",
+	                    Vendor := 1,
+	                    ProductType := 37,
+	                    ProductCode := 21,
+	                    Major := 11,
+	                    Minor := 2,
+	                    PortLabel := ""ENet"",
+	                    NodeAddress := ""192.168.70.101"",
+	                    Mode := 2#0000_0000_0000_0000,
+	                    CompatibleModule := 0,
+	                    KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>102</ConfigID><FeedbackDevice4>8608</FeedbackDevice4><FeedbackDevice5>0</FeedbackDevice5><FeedbackDevice6>0</FeedbackDevice6><FeedbackDevice7>0</FeedbackDevice7><FeedbackDevice8>0</FeedbackDevice8></public>___]]]
+			ConfigData := [224,1,257,1107296336,14729063,0,10000,16908804,0,257,0,-1035468800,1061158912,1073741824,0,1112014848
+		,0,1120403456,0,1120403456,1124859904,1115815936,1,1,0,0,0,0,0,0,0,8608,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,67108865,16778240
+		,0,0,0,0,0,0,0,0];
+			CONNECTION MotionDiagnostics(Rate := 1000,
+                                          EventID := 0)
+
+                    InputData(Class := Standard) := [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,48,60,3333,3333,10000];
+			END_CONNECTION
+
+            CONNECTION MotionSync(Rate := 0,
+                                   EventID := 0)
+
+            END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734AENTRA = @"	MODULE Pxxxx (Parent := ""IO"",
+	              ParentModPortId := 2,
+	              CatalogNumber := ""1734-AENTR/A"",
+	              Vendor := 1,
+	              ProductType := 12,
+	              ProductCode := 196,
+	              Major := 3,
+	              Minor := 1,
+	              PortLabel := ""ENet"",
+	              ChassisSize := 7,
+	              Slot := 0,
+	              NodeAddress := ""192.168.1.10"",
+	              CommMethod := 805306369,
+	              Mode := 2#0000_0000_0000_0000,
+	              CompatibleModule := 0,
+	              KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>262145</ConfigID></public>___]]]
+			CONNECTION Output(Rate := 20000,
+                               EventID := 0,
+                               Unicast := Yes)
+
+                    InputData(Class := Standard) := [0,0,[0,0,0,0,0,0,0]];
+
+                    OutputData(Class := Standard) := [0,0,[0,0,0,0,0,0,0]];
+			END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        string[] m1734IB8SA = @"	MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-IB8S/A"",
+	            Vendor := 1,
+	            ProductType := 35,
+	            ProductCode := 15,
+	            Major := 1,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 1,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 1,
+	            KeyMask := 2#0000_0000_0001_1111,
+	            SafetyNetwork := 16#0000_3aeb_028d_95d5)
+			ExtendedProp := [[[___<public><ConfigID>102</ConfigID></public>___]]]
+			ConfigData := [86,864,112690911,42937120,15083,33686018,1000,16842752,0,513,16842752,0,513,50397184,0,1025,50397184
+		,0,1025,0,0,0,0];
+			CONNECTION Input(Rate := 80000,
+                              EventID := 0,
+                              TimeoutMultiplier := 1,
+                              NetworkDelayMultiplier := 200,
+                              ReactionTimeLimit := 240,
+                              Unicast := Yes)
+
+                    InputData(Class := Safety) := [0,0,0];
+			END_CONNECTION
+
+            CONNECTION Output(Rate := 30000,
+                               EventID := 0,
+                               TimeoutMultiplier := 2,
+                               NetworkDelayMultiplier := 200,
+                               ReactionTimeLimit := 90.064,
+                               Unicast := Yes)
+
+                    OutputData(Class := Safety) := [0];
+			END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734OB8SA = @"	MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-OB8S/A"",
+	            Vendor := 1,
+	            ProductType := 35,
+	            ProductCode := 16,
+	            Major := 1,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 2,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 1,
+	            KeyMask := 2#0000_0000_0001_1111,
+	            SafetyNetwork := 16#0000_3aeb_028d_95d5)
+			ExtendedProp := [[[___<public><ConfigID>205</ConfigID></public>___]]]
+			ConfigData := [30,864,2015074446,50210104,15083,1000,0,16842752,257];
+			CONNECTION Input(Rate := 80000,
+                              EventID := 0,
+                              TimeoutMultiplier := 1,
+                              NetworkDelayMultiplier := 100,
+                              ReactionTimeLimit := 160,
+                              Unicast := Yes)
+
+                    InputData(Class := Safety) := [0,0];
+			END_CONNECTION
+
+            CONNECTION Output(Rate := 30000,
+                               EventID := 0,
+                               TimeoutMultiplier := 2,
+                               NetworkDelayMultiplier := 200,
+                               ReactionTimeLimit := 90.064,
+                               Unicast := Yes)
+
+                    OutputData(Class := Safety) := [0];
+			END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734IB4DC = @"MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-IB4D/C"",
+	            Vendor := 1,
+	            ProductType := 7,
+	            ProductCode := 307,
+	            Major := 3,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 3,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 0,
+	            KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>262145</ConfigID></public>___]]]
+
+            ConfigData(Class := Standard) := [26,103,1,1000,1000,1000,1000,1000,1000,1000,1000,0,1];
+
+            InputAliasComments(RADIX := Binary);
+        END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734OB4EC = @"	MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-OB4E/C"",
+	            Vendor := 1,
+	            ProductType := 7,
+	            ProductCode := 134,
+	            Major := 3,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 4,
+	            CommMethod := 1073741824,
+	            ConfigMethod := 8388611,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 0,
+	            KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>262156</ConfigID></public>___]]]
+
+            ConfigData(Class := Standard) := [16,123,1,0,0,0,0,0,0,0,0];
+
+            InputAliasComments(RADIX := Binary);
+
+            OutputAliasComments(RADIX := Binary);
+        END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734IE2CC = @"	MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-IE2C/C"",
+	            Vendor := 1,
+	            ProductType := 115,
+	            ProductCode := 24,
+	            Major := 3,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 5,
+	            CommMethod := 536870913,
+	            ConfigMethod := 8388609,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 0,
+	            KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>100</ConfigID></public>___]]]
+
+            ConfigData(Class := Standard) := [46,123,1,3277,16383,0,3113,16547,2867,16793,3,0,1,0,3277,16383,0,3113,16547,2867,16793,3,0,0,2,100];
+			CONNECTION InputData(Rate := 80000,
+                                  EventID := 0,
+                                  Unicast := Yes)
+
+                    InputData(Class := Standard) := [0,0,0,0,0];
+			END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1734OE2CC = @"	MODULE $NoName (Parent := ""Pxxxx"",
+	            ParentModPortId := 1,
+	            CatalogNumber := ""1734-OE2C/C"",
+	            Vendor := 1,
+	            ProductType := 115,
+	            ProductCode := 25,
+	            Major := 3,
+	            Minor := 1,
+	            PortLabel := ""RxBACKPLANE"",
+	            Slot := 6,
+	            CommMethod := 536870913,
+	            ConfigMethod := 8388609,
+	            Mode := 2#0000_0000_0000_0000,
+	            CompatibleModule := 0,
+	            KeyMask := 2#0000_0000_0000_0000)
+			ExtendedProp := [[[___<public><ConfigID>400</ConfigID></public>___]]]
+
+            ConfigData(Class := Standard) := [44,123,1,0,0,1638,8191,-32768,32767,0,1,1,0,1,0,0,0,1638,8191,-32768,32767,0,1,1,0,0,0];
+			CONNECTION OutputData(Rate := 80000,
+                                   EventID := 0,
+                                   Unicast := Yes)
+
+                    InputData(Class := Standard) := [0,0,0];
+
+                    OutputData(Class := Standard) := [0,0];
+			END_CONNECTION
+
+    END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+        string[] m1756L7SP = @"	MODULE USPC_ZZ:Partner (Parent := ""Comms_Pxxxx"",
+	                        ParentModPortId := 1,
+	                        CatalogNumber := ""1756-L7SP"",
+	                        Vendor := 1,
+	                        ProductType := 14,
+	                        ProductCode := 146,
+	                        Major := 24,
+	                        Minor := 1,
+	                        PortLabel := ""RxBACKPLANE"",
+	                        Slot := 12,
+	                        Mode := 2#0000_0000_0000_0000,
+	                        CompatibleModule := 0,
+	                        KeyMask := 2#0000_0000_0000_0000,
+	                        SafetyNetwork := 16#0000_0000_0000_0000)
 	END_MODULE".Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
 
         public Form1()
